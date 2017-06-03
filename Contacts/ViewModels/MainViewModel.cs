2bcf679f892e6656d31786a710d2478e5bacdb89
Zulu <jzuluaga55@gmail.com>
2017-06-03 15:41:24 -0500

    Hasta el paso 19

---
+++ Contacts/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Contacts.ViewModels
+{
+    public class MainViewModel
+    {
+		public ContactsViewModel Contacts
+		{
+			get;
+			set;
+		}
+		
+        public MainViewModel()
+        {
+			Contacts = new ContactsViewModel();
+		}
+    }
+}
